10d713ce1ab098f0020e8583107f5ccf5ea0efa2
Gabriel Soares <gsoaresdesouza@live.com>
2024-03-11 08:24:08 -0300

    Autorização pelo Zuul

--- hr-worker/src/main/java/com/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -48,12 +48,11 @@ public class WorkerResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
-		// try {
-		// Thread.sleep(3000l);
-		// } catch (InterruptedException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
+		try {
+			Thread.sleep(3000l);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 
